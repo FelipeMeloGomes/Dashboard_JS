991f528481646e7a48d70e401adb3b9b24bba10d
Felipe <felipemelogomes36@gmail.com>
2022-09-25 22:54:50 -0300

    dashboard-js

---
+++ js/pedidos.js
@@ -0,0 +1,62 @@
+const Orders = [
+    {
+        productName: 'Mini Drone',
+        productNumber: '34567',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Iphone 13',
+        productNumber: '55535',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Computador',
+        productNumber: '37890',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22211',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Mini Drone',
+        productNumber: '31567',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Mini Drone',
+        productNumber: '39567',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Mini Drone',
+        productNumber: '38567',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Mini Drone',
+        productNumber: '30567',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Mini Drone',
+        productNumber: '32567',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Mini Drone',
+        productNumber: '36667',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
